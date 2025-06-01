908c0f7486202cf5007063c9743f8d240e597e99
Nitish <mail.logicninjax@gmail.com>
2025-06-01 13:35:51 +0530

    add transaction record entity

---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,63 @@
+package com.jpmc.midascore.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID txnId;
+
+    private float amount;
+
+    @ManyToOne
+    private UserRecord sender;
+
+    @ManyToOne
+    private UserRecord receiver;
+
+    public UUID getTxnId() {
+        return txnId;
+    }
+
+    public TransactionRecord() {
+    }
+
+    public TransactionRecord(float amount, UserRecord sender, UserRecord receiver) {
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public void setTxnId(UUID txnId) {
+        this.txnId = txnId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public UserRecord getSender() {
+        return sender;
+    }
+
+    public void setSender(UserRecord sender) {
+        this.sender = sender;
+    }
+
+    public UserRecord getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserRecord receiver) {
+        this.receiver = receiver;
+    }
+}
