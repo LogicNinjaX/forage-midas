ab361bde399f22821dd05a24502f555d07caa447
Nitish <mail.logicninjax@gmail.com>
2025-06-02 10:33:54 +0530

    integrate incentive api

---
+++ src/main/java/com/jpmc/midascore/service/IncentiveService.java
@@ -0,0 +1,24 @@
+package com.jpmc.midascore.service;
+
+import com.jpmc.midascore.entity.dto.Incentive;
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class IncentiveService {
+
+    private final RestTemplate restTemplate;
+
+    public IncentiveService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public float getIncentive(Transaction transaction){
+        String url = "http://localhost:8080/incentive";
+
+        Incentive incentive = restTemplate.postForObject(url, transaction,Incentive.class);
+
+        return (incentive != null) ? incentive.getAmount() : 0.0f;
+    }
+}
